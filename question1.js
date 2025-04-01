ee02bc83511227133727a5bdd41b16b2458cb9a5
The Collective <148755759+Pl-x@users.noreply.github.com>
2025-04-01 13:50:04 +0300

    Update question1.js

--- question1.js
+++ question1.js
@@ -1,3 +1,6 @@
+// Write a program that prints the numbers from 1 to 100. For multiples of 3, print "Fizz"; for
+// multiples of 5, print "Buzz"; and for numbers that are multiples of both 3 and 5, print
+// "FizzBuzz".
 let fizz = []
 let Buzz = []
 let fizzBuzz = []
