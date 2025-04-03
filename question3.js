8ff4ac25d011bdcb11079ceb0ca250b09a4ecad7
The Collective <148755759+Pl-x@users.noreply.github.com>
2025-04-03 11:33:50 +0300

    Update question3.js

--- question3.js
+++ question3.js
@@ -1,8 +1,7 @@
 // Question 3: Power of Two
 // Write a program that takes an integer as input and returns true if the input is a power of two.
- let number = prompt("enter an integer"
+ let number = prompt("enter an integer")
 function ispowerof2(number) {
- )
   return Math.log2(number)%1===0
 }
 alert(number + " : " + ispowerof2(number))
