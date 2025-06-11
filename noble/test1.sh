45762a3e2c3b7ebe3c8e54ba809316fe7cbd275b
The Collective <148755759+Pl-x@users.noreply.github.com>
2025-06-11 21:03:40 +0300

    Delete noble directory

--- noble/test1.sh
+++
@@ -1,8 +0,0 @@
-# Test phone number (sandbox)
-curl -X POST http://localhost:3000/api/payment/mpesa \
-  -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIsImVtYWlsIjoidGVzdEBleGFtcGxlLmNvbSIsImlhdCI6MTc0NzI1MDAyMSwiZXhwIjoxNzQ3MzM2NDIxfQ.GZbC-feNj44rwBuvQvXaStqz94DbRXfzIvV43vLdqHA" \
-  -H "Content-Type: application/json" \
-  -d '{
-    "amount": 100,
-    "phoneNumber": "254794869383"
-  }'
--- noble/test4.sh
+++
@@ -1,7 +0,0 @@
-curl -X POST http://localhost:3000/api/signup \
-  -H "Content-Type: application/json" \
-  -d '{
-    "email": "test@example.com",
-    "password": "password123",
-    "name":"Allan"
-  }'
